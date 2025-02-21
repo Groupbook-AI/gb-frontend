3996e97dac6bf257df5add8c4807ae42534e6563
Dfree001 <130784817+Dfree001@users.noreply.github.com>
2025-02-21 11:05:19 -0500

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer style={{ background: "#333", color: "white", padding: "10px", marginTop: "20px" }}>
+            <p>© 2024 Groupbook. All rights reserved.</p>
+        </footer>
+    );
+}
