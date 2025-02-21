d01a6471f252fad42ed684e730bfb4f4c360de20
Dfree001 <130784817+Dfree001@users.noreply.github.com>
2025-02-21 10:33:45 -0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['tailwindui.com', 'images.unsplash.com'], // Allow images from tailwindui.com
+  },
+}
+
+module.exports = nextConfig
